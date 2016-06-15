c0bf562d412c1c1944bb8be56505493364e4e5fd
Nick McNutt <nmcnutt@utk.edu>
2016-06-15 14:48:03 -0400

    Renamed package from VantagePointTrees to VPTrees

---
+++ src/VPTrees.jl
@@ -0,0 +1,29 @@
+module VantagePointTrees
+
+using Distances
+
+export
+    # construct.jl
+    PeriodicSqEuclidean,
+    evaluate,
+
+    Node,
+    VPTree,
+    partition!,
+    quickselect!,
+    distances!,
+    #workmem
+
+    # query.jl
+    knn!,
+
+    # display.jl
+    printnode,
+    show,
+    display
+
+include("construct.jl")
+include("query.jl")
+include("display.jl")
+
+end
